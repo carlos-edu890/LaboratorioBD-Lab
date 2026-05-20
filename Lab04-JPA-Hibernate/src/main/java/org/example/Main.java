69a278029a2eda7474db64276918069e910ad194
Carlos Eduardo <cacadudugerlop@gmail.com>
2026-05-19 21:29:14 -0300

    Adicionando dependencias e o persistence.xml

--- Lab04-JPA-Hibernate/src/main/java/org/example/Main.java
+++ Lab04-JPA-Hibernate/src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            // Substitua "NOME_DA_SUA_PU" pelo nome que está no seu <persistence-unit name="...">
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("Lab04JpaHibernate");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("Conexão realizada com sucesso!");
+            emf.close();
+        } catch (Exception e) {
+            System.err.println("Erro na configuração ou conexão:");
+            e.printStackTrace();
         }
     }
 }
